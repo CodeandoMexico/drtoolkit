c90e17ec9dab2d78349a1781b3395fb7499b4d2f
Hans <hnsdream@gmail.com>
2018-07-31 21:10:53 -0500

    title

--- server.js
+++ server.js
@@ -43,4 +43,4 @@ app.get('/projects', (req, res)=> {
   })
 })
 
-app.listen(process.env.PORT, () => console.log('Listening on port 3000!'))
+app.listen(process.env.PORT || 3000, () => console.log('App started!'))
