b0ca7cf956024c5e175dd9d40d62dd29494ea681
Giovanni Mendoza <mendozagioo@gmail.com>
2018-09-14 01:14:18 -0500

    Use of autoprefixier within precompilation of CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 'use strict'
 var gulp = require('gulp')
 var sass = require('gulp-sass')
+var autoprefixer = require('gulp-autoprefixer')
 
 gulp.task('sass', function () {
   return gulp.src('src/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(gulp.dest('public/css/'))
 })
 
